819803c75ba50d24cad0adb268814639eea3c44d
Mike McNeeley <mmcneeley@gmail.com>
2018-03-02 18:59:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ sentence = "the lazy dog"
 
 def reverse_each_word(sentence)
   #def reverse(sentence_array)
-    sentence.collect do |word|
+    sentence.split.collect do |word|
       word.reverse
     end.join(" ")
   #end
